0dce22f6c374505556be84dd36e3e030488dd1cb
AureLey <110115882+AureLey@users.noreply.github.com>
2023-05-31 19:59:44 +0200

    Creation of ControllersTest and some basics test, installation of php cs-fixer not working PHP version 5.6 not supported

---
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class UserControllerTest extends WebTestCase
+{
+    protected static $client;
+
+    public static function setUpBeforeClass()
+    {
+        self::$client = static::createClient();
+    }
+
+    public function testlistAction()
+    {       
+        $crawler = self::$client->request('GET', '/users');
+
+        $this->assertEquals(200, self::$client->getResponse()->getStatusCode());      
+
+    }
+
+    public function testcreateAction()
+    {        
+        $crawler = self::$client->request('GET', '/users/create');
+
+        $this->assertEquals(200, self::$client->getResponse()->getStatusCode());        
+
+    }
+
+    public function testeditAction()
+    {     
+        $crawler = self::$client->request(Request::METHOD_GET, '/users/1/edit'); 
+        $this->assertEquals(200, self::$client->getResponse()->getStatusCode());   
+    }
+
+}
