6797964cc3163355c6954a6a229f941b81e4099e
AureLey <110115882+AureLey@users.noreply.github.com>
2023-06-05 19:16:08 +0200

    Step 8&9 SymfCast, moving twig in template, Class in Entity and modify namespace AppBunde to App

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,7 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
