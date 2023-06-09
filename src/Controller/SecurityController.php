1efc370b8a4bf028bf5d0fbb05babfce5b22af9e
AureLey <110115882+AureLey@users.noreply.github.com>
2023-06-09 11:18:38 +0200

    upgrade 6.0 and modify security to test login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -32,11 +32,11 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    #[Route('/login_check', name: 'login_check')]
-    public function loginCheck()
-    {
-        // This code is never executed.
-    }
+    // #[Route('/login_check', name: 'login_check')]
+    // public function loginCheck()
+    // {
+    //     // This code is never executed.
+    // }
 
     #[Route('/logout', name: 'logout')]
     public function logoutCheck()
