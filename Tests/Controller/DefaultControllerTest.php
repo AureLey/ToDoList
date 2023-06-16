bacaa3e8cbc660575ea9782d1dfaaabb11cb8de8
AureLey <110115882+AureLey@users.noreply.github.com>
2023-06-16 09:33:51 +0200

    creation of test files

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -2,17 +2,42 @@
 
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Tests\Tools\ToolsWebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultControllerTest extends WebTestCase
-{
+
+class DefaultControllerTest extends ToolsWebTestCase
+{   
     
+        
+    /**
+     * testHomepageRedirection, Testing redirection to login route
+     *
+     * @return void
+     */
     public function testHomepageRedirection(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
-
-        //$this->assertResponseIsSuccessful();
+        $this->client->request(Request::METHOD_GET, '/');
+        //Test Redirection when nobody is loged
         $this->assertResponseRedirects();
+        //Follow Redirection
+        $this->client->followRedirect();
+        //Testing redirect Route
+        $this->assertRouteSame('login');        
+
+    }        
+
+    public function testHomepageWithAuth():void
+    {   
+        $this->client->loginUser($this->getUserTest()); 
+
+        $crawler = $this->client->request(Request::METHOD_GET, '/');      
+        //Testing redirect Route
+        $this->assertRouteSame('homepage');
+        //$this->assertSelectorTextSame('title', 'To Do List app');
+        $this->assertSelectorTextSame('h1', 'Bienvenue sur Todo List, l\'application vous permettant de gérer l\'ensemble de vos tâches sans effort !');
+       
     }
+
 }
