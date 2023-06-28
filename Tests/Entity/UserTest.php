0d0e614b6c68ead718a220a71012f9be644dc2f4
AureLey <110115882+AureLey@users.noreply.github.com>
2023-06-28 18:10:14 +0200

    add php cs fixer composer files

--- Tests/Entity/UserTest.php
+++ Tests/Entity/UserTest.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Todolist
- *
- * (c)
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace App\Tests\Entity;
 
 use App\Entity\Task;
@@ -18,11 +9,9 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserTest extends KernelTestCase
-{    
+{
     /**
      * testUser, fill one user with correct infos and checking Errors.
-     *
-     * @return void
      */
     public function testUser(): void
     {
@@ -34,20 +23,19 @@ class UserTest extends KernelTestCase
             ->setEmail('testUser.test@gmail.com')
             ->setPassword('password')
             ->setRoles(['ROLE_USER']);
-        $this->assertEquals($user->getUsername(),$user->getUserIdentifier());
+        $this->assertSame($user->getUsername(), $user->getUserIdentifier());
         // Testing AddTask
         $user->addTask($task);
-        $this->assertCount(1,$user->getTasks());
+        $this->assertCount(1, $user->getTasks());
         // Testing RemoveTask
         $user->removeTask($task);
-        $this->assertCount(0,$user->getTasks());
+        $this->assertCount(0, $user->getTasks());
         // Testing Entity
         $errors = $container->get('validator')->validate($user);
-        $this->assertCount(0,$errors);
+        $this->assertCount(0, $errors);
 
         // Testing setRoles
         $user->setRoles([]);
-        $this->assertEquals('ROLE_USER', $user->getRoles()[0]);
-
+        $this->assertSame('ROLE_USER', $user->getRoles()[0]);
     }
 }
